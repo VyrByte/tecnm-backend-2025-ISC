3e56a30c4e471c39d4dea6e8371e2b1a8d29ff81
Ricardo Soto <75344350+VyrByte@users.noreply.github.com>
2025-12-09 22:10:48 +0000

    fix: Error con columna inexistente corregido

--- src/main/java/mx/tecnm/ecommerce/api/repository/ProductoDAO.java
+++ src/main/java/mx/tecnm/ecommerce/api/repository/ProductoDAO.java
@@ -1,12 +1,13 @@
+//mx/tecnm/ecommerce/api/repository/ProductoDAO.java
 package mx.tecnm.ecommerce.api.repository;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
-import mx.tecnm.ecommerce.api.dto.PUTProductoDTO;
+import mx.tecnm.ecommerce.api. dto.PUTProductoDTO;
 import mx. tecnm.ecommerce.api.dto.ProductoDTO;
-import mx.tecnm.ecommerce. api.models.Producto;
+import mx.tecnm.ecommerce.api. models.Producto;
 
 @Repository
 public class ProductoDAO {
@@ -14,7 +15,7 @@ public class ProductoDAO {
     private JdbcClient jdbcClient;
 
     public List<Producto> obtenerProductos() {
-        String sql = "SELECT id, nombre, precio, sku, color, marca, descripcion, peso, alto, ancho, profundidad, categorias_id FROM productos WHERE activo=true";
+        String sql = "SELECT id, nombre, precio, sku, color, marca, descripcion, peso, alto, ancho, profundidad, categorias_id FROM productos";
         return jdbcClient.sql(sql)
                 .query(new ProductoRM())
                 .list();
@@ -26,11 +27,37 @@ public class ProductoDAO {
                 .param("id", id)
                 .query(new ProductoRM())
                 .list();
-        return productos. isEmpty() ? null : productos.get(0);
+        return productos.isEmpty() ? null : productos. get(0);
     }
 
     public Producto insertarProducto(ProductoDTO producto) {
-        int filas = jdbcClient. sql("INSERT INTO productos(nombre, precio, sku, color, marca, descripcion, peso, alto, ancho, profundidad, categorias_id) VALUES (:nombre, :precio, :sku, : color, :marca, :descripcion, :peso, :alto, : ancho, :profundidad, :categorias_id) RETURNING id")
+        int nuevoId = jdbcClient.sql("INSERT INTO productos(nombre, precio, sku, color, marca, descripcion, peso, alto, ancho, profundidad, categorias_id) VALUES (:nombre, :precio, : sku, :color, :marca, :descripcion, :peso, : alto, :ancho, :profundidad, :categorias_id) RETURNING id")
+            .param("nombre", producto.nombre())
+            .param("precio", producto.precio())
+            .param("sku", producto.sku())
+            .param("color", producto.color())
+            .param("marca", producto.marca())
+            .param("descripcion", producto.descripcion())
+            .param("peso", producto.peso())
+            .param("alto", producto. alto())
+            .param("ancho", producto.ancho())
+            .param("profundidad", producto.profundidad())
+            .param("categorias_id", producto.categorias_id())
+            .query((rs, rowNum) -> rs.getInt("id"))
+            .single();
+        return obtenerProductoPorId(nuevoId);
+    }
+
+    public boolean eliminarProducto(int id) {
+        int filas = jdbcClient.sql("DELETE FROM productos WHERE id=:id")
+            .param("id", id)
+            .update();
+        return filas > 0;
+    }
+
+    public Producto actualizarProducto(int id, PUTProductoDTO producto) {
+        int filas = jdbcClient.sql("UPDATE productos SET nombre=:nombre, precio=:precio, sku=:sku, color=: color, marca=:marca, descripcion=:descripcion, peso=:peso, alto=:alto, ancho=:ancho, profundidad=:profundidad WHERE id=:id")
+            .param("id", id)
             .param("nombre", producto.nombre())
             .param("precio", producto.precio())
             .param("sku", producto.sku())
@@ -39,37 +66,9 @@ public class ProductoDAO {
             .param("descripcion", producto.descripcion())
             .param("peso", producto.peso())
             .param("alto", producto.alto())
-            .param("ancho", producto. ancho())
-            .param("profundidad", producto.profundidad())
-            .param("categorias_id", producto.categorias_id())
-            .query((rs, rowNum) -> rs.getInt("id"))
-            .single();
-        return obtenerProductoPorId(filas);
-    }
-
-    public Producto desactivarProducto(int id) {
-        int filas = jdbcClient.sql("UPDATE productos SET activo=false WHERE id=: id RETURNING id")
-            .param("id", id)
-            .query((rs, rowNum) -> rs.getInt("id"))
-            .single();
-        return filas > 0 ? obtenerProductoPorId(id) : null;
-    }
-
-    public Producto actualizarProducto(int id, PUTProductoDTO producto) {
-        int filas = jdbcClient.sql("UPDATE productos SET nombre=:nombre, precio=:precio, sku=:sku, color=:color, marca=: marca, descripcion=:descripcion, peso=:peso, alto=:alto, ancho=:ancho, profundidad=:profundidad WHERE id=:id RETURNING id")
-            .param("id", id)
-            .param("nombre", producto.nombre())
-            .param("precio", producto.precio())
-            .param("sku", producto.sku())
-            .param("color", producto.color())
-            .param("marca", producto. marca())
-            .param("descripcion", producto.descripcion())
-            .param("peso", producto. peso())
-            .param("alto", producto.alto())
             .param("ancho", producto.ancho())
             .param("profundidad", producto.profundidad())
-            .query((rs, rowNum) -> rs.getInt("id"))
-            .single();
+            .update();
         return filas > 0 ? obtenerProductoPorId(id) : null;
     }
 }
