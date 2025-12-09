db1e17ce5341b11d9ce9dd4c9bb3d1bfe620b887
Juan Zaragoza <juan.zaragoza.est70@gmail.com>
2025-12-09 16:59:35 +0000

    feat: Agregado el DAO CategoriaDAO con operaciones CRUD

---
+++ src/main/java/mx/tecnm/ecommerce/api/repository/CategoriaDAO.java
@@ -0,0 +1,55 @@
+package mx.tecnm. ecommerce.api.repository;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import mx.tecnm.ecommerce.api.models. Categoria;
+
+@Repository
+public class CategoriaDAO {
+    @Autowired
+    private JdbcClient jdbcClient;
+
+    public List<Categoria> obtenerCategorias() {
+        String sql = "SELECT id, nombre FROM categorias WHERE activo=true";
+        return jdbcClient.sql(sql)
+                .query(new CategoriaRM())
+                .list();
+    }
+
+    public Categoria obtenerCategoriaPorId(int id) {
+        String sql = "SELECT id, nombre FROM categorias WHERE id = : id";
+        List<Categoria> categorias = jdbcClient.sql(sql)
+                .param("id", id)
+                .query(new CategoriaRM())
+                .list();
+        
+        return categorias.isEmpty() ? null : categorias.get(0);
+    }
+
+    public Categoria insertarCategoria(String categoria) {
+        String sql = "INSERT INTO categorias (nombre) VALUES (:nombre) RETURNING id, nombre";
+        return jdbcClient.sql(sql)
+                .param("nombre", categoria)
+                .query(new CategoriaRM())
+                .single();
+    }
+
+    public Categoria actualizarCategoria(int id, String nombre) {
+        String sql = "UPDATE categorias SET nombre = :nombre WHERE id = : id RETURNING id, nombre";
+        return jdbcClient.sql(sql)
+                .param("id", id)
+                .param("nombre", nombre)
+                .query(new CategoriaRM())
+                .single();
+    }
+
+    public Categoria desactivarCategoria(int id) {
+        String sql = "UPDATE categorias SET activo = false WHERE id = :id RETURNING id, nombre";
+        return jdbcClient.sql(sql)
+                .param("id", id)
+                .query(new CategoriaRM())
+                .single();
+    }
+}
