b1abb9ddc318e1ec53ef58758e398f7cc30ce3e1
Ricardo Soto <vyrbyte@gmail.com>
2025-12-09 10:32:45 -0600

    feat: Agregado el DTO PUTProductoDTO para actualizaciones

---
+++ src/main/java/mx/tecnm/ecommerce/api/dto/PUTProductoDTO.java
@@ -0,0 +1,34 @@
+package mx.tecnm.ecommerce.api.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PUTProductoDTO(
+        @NotBlank(message = "El nombre es obligatorio")
+        @Size(max = 60, message = "El nombre no puede tener más de 60 caracteres")
+        String nombre,
+
+        @DecimalMin(value = "0.0", inclusive = false, message = "El precio debe ser mayor a 0")
+        float precio,
+
+        @NotBlank(message = "El SKU es obligatorio")
+        @Size(max = 15, message = "El SKU no puede tener más de 15 caracteres")
+        String sku,
+
+        @Size(max = 15)
+        String color,
+
+        @NotBlank(message = "La marca es obligatoria")
+        @Size(max = 20, message = "La marca no puede tener más de 20 caracteres")
+        String marca,
+
+        String descripcion,
+
+        @DecimalMin(value = "0.0", message = "El peso no puede ser negativo")
+        float peso,
+
+        float alto,
+        float ancho,
+        float profundidad
+) {}
