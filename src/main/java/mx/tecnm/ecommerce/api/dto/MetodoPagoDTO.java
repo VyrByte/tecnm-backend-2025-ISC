664a84820d5c7acafb76b1f0d858540872998c71
Ricardo Soto <vyrbyte@gmail.com>
2025-12-09 10:23:20 -0600

    feat: Agregado DTO de Método de Pago

--- src/main/java/mx/tecnm/ecommerce/api/dto/MetodoPagoDTO.java
+++ src/main/java/mx/tecnm/ecommerce/api/dto/MetodoPagoDTO.java
@@ -1,4 +1,14 @@
 package mx.tecnm.ecommerce.api.dto;
 
-public class MetodoPagoDTO {
-}
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record MetodoPagoDTO(
+        @NotBlank(message = "El nombre es obligatorio")
+        @Size(max = 25, message = "El nombre no puede tener más de 25 caracteres")
+        String nombre,
+
+        @DecimalMin(value = "0.0", message = "La comisión no puede ser negativa")
+        float comision
+) {}
